c7f209eb9837b2c0adf1f9a2b5fa6837b09e94e6
Tristan <erk0002@hz.nl>
2024-11-21 12:16:19 +0100

    minor color changes

--- src/lib/components/ToggleButtons.svelte
+++ src/lib/components/ToggleButtons.svelte
@@ -7,7 +7,7 @@
     }
 </script>
 
-<div class="flex justify-center items-center  absolute">
+<div class="flex justify-center items-center absolute">
   <button
     class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => toggle("comms")}
@@ -23,7 +23,7 @@
     <i class="fa-solid fa-magnifying-glass text-4xl"></i>
   </button>
   <button
-    class="m-2 w-16 h-16 absolute bottom-[3rem] text-base border-0 rounded-full cursor-pointer text-white bg-primary-500 dark:bg-primary-dark-500"
+    class="m-2 w-16 h-16 absolute bottom-[3rem] text-base border-0 rounded-full cursor-pointer text-background bg-primary-500 dark:bg-primary-dark-500 dark:text-background-dark hover:bg-primary-900 dark:hover:bg-primary-dark-900 dark:hover:text-text-dark hover:text-primary-500"
     aria-label="Add">
     <i class="fa-solid fa-plus text-4xl"></i>
   </button>
