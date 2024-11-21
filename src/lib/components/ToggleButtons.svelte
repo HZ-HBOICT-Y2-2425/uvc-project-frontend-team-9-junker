747ff1e31aaa5fd230b7bab083164460f23aaae7
Tristan <erk0002@hz.nl>
2024-11-21 11:51:52 +0100

    minor togglestate changes

--- src/lib/components/ToggleButtons.svelte
+++ src/lib/components/ToggleButtons.svelte
@@ -10,17 +10,17 @@
 <div class="flex justify-center items-center  absolute">
   <button
     class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
+    on:click={() => toggle("comms")}
+    class:active={$toggleState === "comms"}
+    aria-label="Communities">
+    <i class="fa-solid fa-users text-4xl"></i>
+  </button>
+  <button
+    class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => toggle("search")}
     class:active={$toggleState === "search"}
     aria-label="Search">
     <i class="fa-solid fa-magnifying-glass text-4xl"></i>
-  </button>
-  <button
-    class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
-    on:click={() => toggle("comms")}
-    class:is-active={$toggleState === "comms"}
-    aria-label="Communities">
-    <i class="fa-solid fa-users text-4xl"></i>
   </button>
   <button
     class="m-2 w-16 h-16 absolute bottom-[3rem] text-base border-0 rounded-full cursor-pointer text-white bg-primary-500 dark:bg-primary-dark-500"
@@ -32,16 +32,16 @@
   <button
     class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => toggle("chats")}
-    class:is-active={$toggleState === "chats"}
+    class:active={$toggleState === "chats"}
     aria-label="Chats">
     <i class="fa-solid fa-comment text-4xl"></i>
   </button>
   <button
     class="m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => toggle("items")}
-    class:is-active={$toggleState === "items"}
+    class:active={$toggleState === "items"}
     aria-label="Items">
-    <i class="fa-solid fa-shop text-4xl"></i>
+    <i class="fa-solid fa-list text-4xl"></i>
   </button>
 </div>
 
