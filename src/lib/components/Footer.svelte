e13ce7e5e25242eb16b9fd899f057055dbb1e207
Tristan <erk0002@hz.nl>
2024-11-23 01:21:05 +0100

    added community component with filler content and code input area

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,18 +1,18 @@
 <script>
-    import ToggleButtons from "./ToggleButtons.svelte";
-    import { fly } from 'svelte/transition';
-    import { page } from '$app/stores';
+  import ToggleButtons from "./ToggleButtons.svelte";
+  import { fly } from 'svelte/transition';
+  import { page } from '$app/stores';
 
-    let showFooter = true;
+  let showFooter = true;
 
-    $: {
-        const path = $page.url.pathname;
-        showFooter = !(path === '/username' || path === '/notifications');
-    }
+  $: {
+    const path = $page.url.pathname;
+    showFooter = !(path === '/username' || path === '/notifications');
+  }
 </script>
 
-<div transition:fly="{{ y: 100, duration: 300 }}" class="h-full w-full bg-background dark:bg-background-dark border-t-4 border-solid border-secondary-500 dark:border-secondary-dark-500 shadow-[0vh_-4vh_16vh_rgba(0,0,0,0.5)] flex flex-nowrap justify-center" class:hidden={!showFooter}>
-    <ToggleButtons />
+<div transition:fly="{{ y: 100, duration: 300 }}" class="fixed bottom-0 z-20 h-[10vh] left-0 w-full bg-background dark:bg-background-dark border-t-4 border-solid border-secondary-500 dark:border-secondary-dark-500 shadow-[0vh_-4vh_16vh_rgba(0,0,0,0.1)] flex flex-nowrap justify-center" class:hidden={!showFooter}>
+  <ToggleButtons />
 </div>
 
 <style>
