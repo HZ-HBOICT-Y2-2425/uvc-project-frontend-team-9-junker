f992a1c64cdcfe6ba86d2a2445ba5605a6f4e416
Frederic Borowiak <frenibo@gmail.com>
2024-11-25 02:30:10 +0100

    card sort works!

--- src/routes/community/[id]_[title]/+page.svelte
+++ src/routes/community/[id]_[title]/+page.svelte
@@ -1,36 +1,112 @@
 <script>
 // @ts-nocheck
     import SubHeaderV2 from "$lib/components/SubHeaderV2.svelte";
+    import CommunityListingCard from "$lib/components/CommunityListings/CommunityListingCard.svelte";
+    import { darkModeEnabled } from "$lib/stores/AllPurposeStore";
     import { Community } from "$lib/models/Community.js";
     import { page } from "$app/stores";
     import { communityObject } from "$lib/stores/AllPurposeStore";
     import ViewCommunityListings from "$lib/components/ViewCommunityItems.svelte";
+    import { onMount } from "svelte";
     let communityId = $page.params.id;
-    let title = `Listings: ${$page.params.title}`;
+    let communityName = $page.params.title;
+    let title = `Listings`;
     export let data;
+    let itemsArray = data.items;
+
+    let sortParams = ["name", "views", "interested"];
+    let sortBy = {col: "name", ascending: false};
+
+    $: sort = (column) => {
+		
+		if (sortBy.col == column) {
+			sortBy.ascending = !sortBy.ascending
+		} else {
+			sortBy.col = column
+			sortBy.ascending = true
+		}
+		
+		// Modifier to sorting function for ascending or descending
+		let sortModifier = (sortBy.ascending) ? 1 : -1;
+		
+		let sort = (a, b) => 
+			(a[column] < b[column]) 
+			? -1 * sortModifier 
+			: (a[column] > b[column]) 
+			? 1 * sortModifier 
+			: 0;
+		
+		itemsArray = itemsArray.sort(sort);
+	}
+
+    onMount(() => {
+        if(sortParams) {
+            sort(sortParams[0]);
+        }
+    });
 
 </script>
 
-<header>
-    <SubHeaderV2 title={title}/>
-</header>
 
-<section class="container">
-	<h2 class="text-center text-2xl my-6">Listings</h2>
-	<table class="table-auto border-separate w-screen text-xs text-center">
-		<thead><tr><th>ID</th><th>UserID</th><th>Name</th><th>Description</th><th>Views</th></tr></thead>
-		<tbody>
-			{#each data.items as item}
-				<tr class="odd:bg-white even:bg-slate-50 hover:bg-orange-50 hover:cursor-pointer">
-                    <td class="p-3">{item.id}</td>
-                    <td>{item.userid}</td>
-                    <td>{item.name}</td>
-                    <td>{item.description}</td>
-                    <td>{item.views}</td></tr
-				>
-			{/each}
-		</tbody>
-	</table>
-</section>
+<div class:dark={$darkModeEnabled} class="bg-background dark:bg-background-dark h-screen w-screen flex flex-col flex-nowrap">
+    <header>
+        <SubHeaderV2 title={title}/>
+    </header>
 
-<ViewCommunityListings communityId={communityId} communityMock={$communityObject}/>
+    <!-- Divider with Community Name -->
+    <div class="flex items-center w-full max-w-md">
+        <hr class="border-2 rounded flex-grow border-secondary-500 dark:border-secondary-dark-500" />
+        <span class="mx-4 text-text dark:text-text-dark font-medium">{communityName}</span>
+        <hr class="border-2 rounded flex-grow border-secondary-500 dark:border-secondary-dark-500" />
+        </div>
+
+        <!--TODO: Get rid of top margin-->
+        <sort-bar class="w-full border-t-[1px] border-b-[1px] border-black border-solid flex flex-row flex-nowrap justify-evenly">
+            {#each sortParams as param}
+                <button class="" on:click={() => sort(`${param}`)} aria-label="Sort by {param}">
+                    {param}
+                    {#if sortBy.col === `${param}`}
+                        {#if sortBy.ascending == true}
+                            <i class="m-0 text-secondary-500 dark:text-secondary-dark-500 m-4 fa-solid fa-sort-down"></i>
+                        {:else if sortBy.ascending == false}
+                            <i class="m-0 text-secondary-500 dark:text-secondary-dark-500 m-4 fa-solid fa-sort-up"></i>
+                        {/if}
+                    {:else}
+                        <i class="m-0 text-secondary-500 dark:text-secondary-dark-500 m-4 fa-solid fa-sort-up fa-blank"></i>
+                    {/if}
+                </button>
+            {/each}
+        </sort-bar>
+    <div class="text-text dark:text-text-dark overflow-y-auto" style="max-height: 100vh;">
+        
+
+        <div class="flex flex-col items-center p-4 space-y-8">
+    
+            
+        
+            <!-- Community Grid -->
+            <!--TODO: Get rid of top margin-->
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 w-full">
+                <!-- Community Cards -->
+                {#each itemsArray as item}
+                <CommunityListingCard
+                    item = {item}
+                />
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--ViewCommunityListings communityId={communityId} communityMock={$communityObject}/-->
+
+<style>
+    .wrapper {
+        height: 100vh;
+        overflow-y: hidden;
+    }
+    .fa-blank {
+        visibility: hidden !important;
+    }
+</style>
