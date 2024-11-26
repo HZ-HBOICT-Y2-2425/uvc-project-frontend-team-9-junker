4119080113edca9327ff971c3cb89c8d0cc61f78
Frederic Borowiak <frenibo@gmail.com>
2024-11-26 15:17:55 +0100

    CommunityListingCard mostly finished

--- src/lib/components/CommunityListings/CommunityListingCard.svelte
+++ src/lib/components/CommunityListings/CommunityListingCard.svelte
@@ -13,30 +13,85 @@
 </script>
 
 <button
-class="relative bg-background sm:w-[30vh] dark:bg-background-dark rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow cursor-pointer aspect-square"
+class="relative bg-background sm:w-[30vh] dark:bg-background-dark rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow cursor-pointer aspect-[3/4]"
 on:click={() => navigate()}
 >
     <!-- Image Section -->
-    <div class="h-full relative">
-    <!--img src={item.picture} alt={item.name} class="w-full h-full object-cover" /-->
-    <!--Method for importing images: https://stackoverflow.com/questions/77934659/how-can-i-dynamically-import-images-stored-in-lib-within-a-component-in-svelte -->
-    <img src={images[`/src/lib/assets/images/${item.picture}`]} alt={item.name} class="w-full h-full object-cover" />
+    <div class="card-content">
+        <!--img src={item.picture} alt={item.name} class="w-full h-full object-cover" /-->
+        <!--Method for importing images: https://stackoverflow.com/questions/77934659/how-can-i-dynamically-import-images-stored-in-lib-within-a-component-in-svelte -->
+        <!--img src={images[`/src/lib/assets/images/${item.picture}`]} alt={item.name} class="w-full h-full object-cover" /-->
+        <img src={images[`/src/lib/assets/images/${item.picture}`]} alt={item.name} class="card-image" />
 
 
-    <!-- Uniform Text Overlay -->
-    <div class="absolute inset-x-0 bottom-0 bg-gray-800 bg-opacity-70 text-white p-4 h-[80px] md:h-[100px]">
-        <h3 class="text-sm md:text-lg font-semibold text-center truncate">{item.name}</h3>
-        <div class="mt-2 flex items-center justify-center space-x-4 text-xs md:text-sm">
-        <div class="flex items-center space-x-1">
-            <i class="fa-solid fa-eye text-white dark:text-text-dark"></i>
-            <span>{item.views}</span>
+        <!-- Uniform Text Overlay -->
+        <!--div class="absolute inset-x-0 bottom-0 bg-gray-800 bg-opacity-70 text-white p-4 h-[80px] md:h-[100px]"-->
+        <!--div class="absolute inset-x-0 bottom-0 bg-white bg-opacity-100 text-black p-4 h-[40px] md:h-[100px]">
+            <h3 class="text-sm md:text-lg font-semibold text-center truncate">{item.name}</h3>
+            <div class="mt-2 flex items-center justify-center space-x-4 text-xs md:text-sm">
+            <div class="flex items-center space-x-1">
+                <i class="fa-solid fa-eye text-white dark:text-text-dark"></i>
+                <span>{item.views}</span>
+            </div>
+            <div class="flex items-center space-x-1">
+                <i class="fa-solid fa-exclamation"></i>
+                <span>{item.interested}</span>
+            </div>
+            </div>
+        </div-->
+
+        
+
+        <div class="card-text">
+            <div class="item-name">
+                {item.name}
+            </div>
+            <div class="item-desc">
+                {item.description}
+            </div>
         </div>
-        <div class="flex items-center space-x-1">
-            <i class="fa-solid fa-exclamation"></i>
-            <span>{item.interested}</span>
-        </div>
-        </div>
-    </div>
     </div>
 </button>
+
+<style>
+    .card-content {
+        display: flex;
+        flex-flow: column nowrap;
+        height: 100%;
+        width: 100%;
+        background-color: white;
+    }
+    .card-image {
+        flex-shrink: 0;
+        object-fit:cover;
+        overflow: hidden;
+        flex-grow: 1;
+        flex-shrink: 1;
+    }
+    .card-text {
+        background-color: white;
+        width: 100%;
+        height: fit-content;
+        flex-basis: content;
+        display: flex;
+        flex-flow: column nowrap;
+        overflow: hidden;
+        padding: 0px 3px;
+        flex-grow: 0;
+        flex-shrink: 0;
+    }
+    .item-name {
+        text-align: left;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+    .item-desc {
+        text-align: left;
+        font-size: x-small;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+</style>
     
--- src/routes/community/[id]_[title]/+page.js
+++ src/routes/community/[id]_[title]/+page.js
@@ -3,13 +3,13 @@ import { page } from "$app/stores";
 import { Item } from "$lib/models/Item";
 
 const items = [
-    new Item(0, 0, "Bike", "Cool bike.", "bike.jpg", "", true, 18, 4),
-    new Item(2, 0, "Sneakers", "Cool bike.", "sneakers.jpg", "", true, 28, 1),
-    new Item(3, 0, "Pan", "Cool bike.", "pan.webp", "", true, 8, 5),
-    new Item(4, 0, "Bookshelf", "Cool bike.", "bookshelf.avif", "", true, 1, 0),
-    new Item(5, 0, "Desk", "Cool bike.", "desk.jpg", "", true, 10, 2),
-    new Item(6, 0, "Glass", "Cool bike.", "glass.webp", "", true, 20, 7),
-    new Item(7, 0, "Tennis Racket", "Cool bike.", "tennis_racket.jpg", "", true, 20, 7),
+    new Item(0, 0, "Bike (Barely Used)", "Almost new", "bike.jpg", "", true, 18, 4),
+    new Item(2, 0, "Sneakers [Vintage]", "Size 12", "sneakers.jpg", "", true, 28, 1),
+    new Item(3, 0, "Pan", "", "pan.webp", "", true, 8, 5),
+    new Item(4, 0, "Bookshelf Bookshelf Hardwood 30x15x7", "Hardwood bookshelf dimensions: 30x15x7", "bookshelf.avif", "", true, 1, 0),
+    new Item(5, 0, "Desk", "height: 120cm", "desk.jpg", "", true, 10, 2),
+    new Item(6, 0, "Glass", "self made", "glass.webp", "", true, 20, 7),
+    new Item(7, 0, "Tennis Racket", "I'm trying to cure my tennis elbow.", "tennis_racket.jpg", "", true, 20, 7),
 ];
 
 export const load = async () => {
