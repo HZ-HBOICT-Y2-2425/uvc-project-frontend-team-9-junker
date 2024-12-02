0e5b4f76c36d74ba3634843a0f8672321569b334
hamin <eshhm3514@naver.com>
2024-12-02 15:05:50 +0100

    made the text underneath the icons on footer

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
     export let title = "Default Title"; // Define a prop for the header title
 </script>
 
-<div class="fixed absolute z-20 h-[10vh] w-full bg-background dark:bg-background-dark border-b-4 border-solid border-secondary-500 dark:border-secondary-dark-500 shadow-[0vh_4vh_16vh_rgba(0,0,0,0.1)] flex items-center px-4">
+<div class="fixed z-20 h-[10vh] w-full bg-background dark:bg-background-dark border-b-4 border-solid border-secondary-500 dark:border-secondary-dark-500 shadow-[0vh_4vh_16vh_rgba(0,0,0,0.1)] flex items-center px-4">
   <div class="absolute left-0 flex items-center space-x-4">
     <NotificationBell />
   </div>
--- src/lib/components/ToggleButtons.svelte
+++ src/lib/components/ToggleButtons.svelte
@@ -1,43 +1,50 @@
 <script>
-// @ts-nocheck
-    import { goto } from '$app/navigation';
+  // @ts-nocheck
+      import { goto } from '$app/navigation';
+  
+      const navigate = (route) => {
+          goto(route);
+      };
+  </script>
 
-    const navigate = (route) => {
-        goto(route);
-    };
-</script>
-
-<div class="flex justify-center items-center absolute text-center">
+<div class="flex justify-center items-center absolute text-center w-full">
   <button
-    class="w-16 m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
+    class="flex flex-col justify-center items-center w-16 m-1.5 p-2.5 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background hover:text-background dark:hover:text-text-dark"
     on:click={() => navigate("/community")}
     aria-label="Communities">
-    <i class="fa-solid fa-users text-3xl"></i>
+    <i class="fa-solid fa-users text-3xl"></i> 
+    <div class="text-[0.65rem] mt-0.5 leading-tight">Community</div> <!-- text size -->
   </button>
+
+
   <button
-    class="w-16 m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
+    class="flex flex-col justify-center items-center w-16 m-1.5 p-2.5 text-base border-0 rounded cursor-pointer text-secondary-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => navigate("/search")}
     aria-label="Search">
-    <i class="fa-solid fa-magnifying-glass text-4xl"></i>
+    <i class="fa-solid fa-magnifying-glass text-3xl"></i>
+    <div class="text-[0.65rem] mt-0.5 leading-tight">Search</div>
   </button>
+
   <button
-    class="m-2 w-16 h-16 absolute bottom-[3rem] text-base border-0 rounded-full cursor-pointer text-background bg-primary-500 dark:bg-primary-dark-500 dark:text-background-dark hover:bg-primary-900 dark:hover:bg-primary-dark-900 dark:hover:text-text-dark hover:text-primary-500"
+    class="flex flex-col justify-center items-center m-1.5 w-16 h-16 absolute bottom-[3rem] text-base border-0 rounded-full cursor-pointer text-background bg-primary-500 dark:bg-primary-dark-500 dark:text-background-dark hover:bg-primary-900 dark:hover:bg-primary-dark-900 dark:hover:text-text-dark hover:text-primary-500 z-10"
     on:click={() => navigate("/new-listing")}
     aria-label="Add">
-    <i class="fa-solid fa-plus text-4xl"></i>
+    <i class="fa-solid fa-plus text-3xl"></i>
   </button>
-  <!-- spacer div  -->
-  <div class="w-16 h-16"></div>
+
   <button
-    class="w-16 m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
+    class="flex flex-col justify-center items-center w-16 m-1.5 p-2.5 text-base border-0 rounded cursor-pointer text-secondary-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => navigate("/swipe")}
     aria-label="Swipe">
-    <i class="fa-solid fa-rotate text-4xl"></i>
+    <i class="fa-solid fa-rotate text-3xl"></i>
+    <div class="text-[0.65rem] mt-0.5 leading-tight">Swipe</div>
   </button>
+
   <button
-    class="w-16 m-2 p-3 text-base border-0 rounded cursor-pointer text-secondary-500 dark:text-secondary-dark-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
+    class="flex flex-col justify-center items-center w-16 m-1.5 p-2.5 text-base border-0 rounded cursor-pointer text-secondary-500 bg-background dark:bg-background-dark active:bg-secondary-500 dark:active:bg-secondary-dark-500 hover:bg-secondary-500 dark:hover:bg-secondary-dark-500 active:text-background dark:text-secondary-dark-500 hover:text-background dark:hover:text-text-dark"
     on:click={() => navigate("/chats")}
     aria-label="Chats">
-    <i class="fa-solid fa-comment text-4xl"></i>
+    <i class="fa-solid fa-comment text-3xl"></i>
+    <div class="text-[0.65rem] mt-0.5 leading-tight">Chat</div>
   </button>
 </div>
